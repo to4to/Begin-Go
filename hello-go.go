05250956c008e4c98198b56f3c56532c48651e58
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-03-11 02:16:45 +0530

    channel complete

--- hello-go.go
+++ hello-go.go
@@ -844,6 +844,39 @@ var pl=fmt.Println;
 //Channels: to communicate between go routines
 
 
+// func nums1(channel chan int){
+
+// channel <- 1
+// channel <- 2
+// channel <- 3
+// }
+// //chan able to send and receive value
+
+
+// func nums2(channel chan int){
+
+// 	channel <- 4
+// 	channel <- 5
+// 	channel <- 6
+// 	}
+
+// func main(){
+
+// channel1:=make(chan int)
+// channel2:=make(chan int)
+// go nums1(channel1)
+// go nums2(channel2)
+// pl(<-channel1)
+// pl(<-channel1)
+// pl(<-channel1)
+
+// pl(<-channel2)
+// pl(<-channel2)
+// pl(<-channel2)
+// }
 
 
 
+
+
+//Mutex/lock
